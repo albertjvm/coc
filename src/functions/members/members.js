e21709046bb67f7858ff6b5604c0f40638d1b080
Albert VanderMeulen <albertjvm@users.noreply.github.com>
2019-12-31 11:56:15 -0500

    fix function

--- src/functions/members/members.js
+++ src/functions/members/members.js
@@ -13,7 +13,7 @@ exports.handler = function(event, context, callback) {
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
-      body: data
+      body: JSON.stringify(data)
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
